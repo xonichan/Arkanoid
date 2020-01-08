6b216da0008972de23f41b5cda89b67a93f5ffde
Nicholas <nicholasrogov@gmail.com>
2020-01-09 00:18:41 +0300

    Test commit

---
+++ app/src/main/java/com/example/arkanoid/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.arkanoid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView web = (WebView)findViewById(R.id.webView);
+        web.getSettings().setJavaScriptEnabled(true);
+        web.setWebChromeClient(new WebChromeClient());
+        web.loadUrl("file:///android_asset/www/index.html");
+    }
+}
